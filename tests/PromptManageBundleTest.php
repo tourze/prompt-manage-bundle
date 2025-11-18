2d54e892aa25fd5fcefa4de36305c98a397e4fa7
github-ci <you@example.com>
2025-11-18 23:23:10 +0800

    fix error

--- tests/PromptManageBundleTest.php
+++ tests/PromptManageBundleTest.php
@@ -2,10 +2,8 @@
 
 namespace Tourze\PromptManageBundle\Tests;
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
-use Symfony\Bundle\MonologBundle\MonologBundle;
 use Tourze\PHPUnitSymfonyKernelTest\AbstractBundleTestCase;
 use Tourze\PromptManageBundle\PromptManageBundle;
 
@@ -16,14 +14,4 @@ use Tourze\PromptManageBundle\PromptManageBundle;
 #[RunTestsInSeparateProcesses]
 class PromptManageBundleTest extends AbstractBundleTestCase
 {
-    public function testGetBundleDependencies(): void
-    {
-        $dependencies = PromptManageBundle::getBundleDependencies();
-
-        $this->assertIsArray($dependencies);
-        $this->assertArrayHasKey(DoctrineBundle::class, $dependencies);
-        $this->assertArrayHasKey(MonologBundle::class, $dependencies);
-        $this->assertEquals(['all' => true], $dependencies[DoctrineBundle::class]);
-        $this->assertEquals(['all' => true], $dependencies[MonologBundle::class]);
-    }
 }
